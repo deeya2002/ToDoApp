bd6ce35834f1e8e8343790e46f5d1790718e420a
deeya2002 <diyadangol2002@gmail.com>
2023-12-31 09:15:43 +0545

    first commit

---
+++ lib/features/todoapp/presentation/view/to_do_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ToDoApp extends StatefulWidget {
+  const ToDoApp({super.key});
+
+  @override
+  State<ToDoApp> createState() => _ToDoAppState();
+}
+
+class _ToDoAppState extends State<ToDoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
